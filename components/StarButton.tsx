ad4c3db12b277d5c6cfec02b90067f214e215bad
yukiHaga <oceansthirteen7510@gmail.com>
2022-09-15 15:26:03 +0900

    star

--- components/StarButton.tsx
+++ components/StarButton.tsx
@@ -3,7 +3,12 @@ import { SearchRepositoriesQuery } from "../types/generated/graphql";
 const StarButton = ({ node }: { node: any }) => {
   const totalCount = node.stargazers.totalCount;
   const buttonText = totalCount === 1 ? "1 Star" : `${totalCount} stars`;
-  return <button>{buttonText}</button>;
+  const viewerHasStarred = node.viewerHasStarred;
+  return (
+    <button>
+      {buttonText} | {viewerHasStarred ? "starred" : "-"}
+    </button>
+  );
 };
 
 export default StarButton;
