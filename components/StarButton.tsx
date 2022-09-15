ab9c502e4bce94da3f34dba7d1b86752ac99bd80
yukiHaga <oceansthirteen7510@gmail.com>
2022-09-15 17:31:13 +0900

    Add: mutation

--- components/StarButton.tsx
+++ components/StarButton.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
-import { AddStarMutation } from "../types/generated/graphql";
-import { ADD_STAR } from "../queries/queries";
+import {
+  AddStarMutation,
+  RemoveStarMutation,
+} from "../types/generated/graphql";
+import { ADD_STAR, REMOVE_STAR } from "../queries/queries";
 import { useMutation } from "@apollo/client";
 
 const StarButton = ({ node }: { node: any }) => {
@@ -10,22 +13,38 @@ const StarButton = ({ node }: { node: any }) => {
     viewerHasStarred: node.viewerHasStarred,
   });
   const [addStar] = useMutation<AddStarMutation>(ADD_STAR);
+  const [removeStar] = useMutation<RemoveStarMutation>(REMOVE_STAR);
 
   const handleClick = async () => {
     try {
-      await addStar({
-        variables: {
-          input: {
-            starrableId: node.id,
+      if (buttonState.viewerHasStarred) {
+        await removeStar({
+          variables: {
+            input: {
+              starrableId: node.id,
+            },
           },
-        },
-      }).then((result) =>
-        setButtonState((prev) => ({
-          ...prev,
-          text: totalCount === 1 ? "1 Star" : `${totalCount} stars`,
-          viewerHasStarred: result.data?.addStar?.starrable?.viewerHasStarred,
-        }))
-      );
+        }).then((result) => {
+          setButtonState((prev) => ({
+            ...prev,
+            viewerHasStarred:
+              result.data?.removeStar?.starrable?.viewerHasStarred,
+          }));
+        });
+      } else {
+        await addStar({
+          variables: {
+            input: {
+              starrableId: node.id,
+            },
+          },
+        }).then((result) => {
+          setButtonState((prev) => ({
+            ...prev,
+            viewerHasStarred: result.data?.addStar?.starrable?.viewerHasStarred,
+          }));
+        });
+      }
     } catch (err) {
       // @ts-ignore
       alert(err.message);
